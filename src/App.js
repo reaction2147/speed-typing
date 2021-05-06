76d2b43266870c116fb542a8eda2f914045cc6e1
William Ford <wford1.home@gmail.com>
2021-05-06 13:57:17 +0100

    Using hooks, track state of the text area and update state on every key stroke

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    const {value} = e.target
+    setText(value);
+  }
+
+  console.log(text)
+
   return (
     <>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea onChange={handleChange} value={text} />
       <h4>Time reminaing: ???</h4>
       <button>Start</button>
       <h1>Word count: ???</h1>
